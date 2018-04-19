81753b5ba6dd9a86d7d5991a6e24b231e0568974
John Evans <lgastako@gmail.com>
2018-04-18 23:53:49 -0600

    Add runResourceT to Prelude.

--- src/Dispenser/Prelude.hs
+++ src/Dispenser/Prelude.hs
@@ -7,30 +7,31 @@ module Dispenser.Prelude
     , sleep
     ) where
 
-import Focus.Prelude               as Exports
+import Focus.Prelude                as Exports
 
-import Control.Foldl               as Exports ( Fold( Fold )
-                                              , FoldM( FoldM )
-                                              )
-import Control.Lens                as Exports ( (^.)
-                                              , makeClassy
-                                              , makeClassyPrisms
-                                              , makeFields
-                                              , makeLenses
-                                              , view
-                                              )
-import Control.Monad.Trans.Control as Exports ( MonadBaseControl )
-import Data.Aeson                  as Exports ( FromJSON
-                                              , Result( Error
-                                                      , Success
-                                                      )
-                                              , ToJSON
-                                              , Value
-                                              , fromJSON
-                                              , toJSON
-                                              )
-import Data.Time.Clock             as Exports ( UTCTime )
-import System.IO.Unsafe                       ( unsafePerformIO )
+import Control.Foldl                as Exports ( Fold( Fold )
+                                               , FoldM( FoldM )
+                                               )
+import Control.Lens                 as Exports ( (^.)
+                                               , makeClassy
+                                               , makeClassyPrisms
+                                               , makeFields
+                                               , makeLenses
+                                               , view
+                                               )
+import Control.Monad.Trans.Control  as Exports ( MonadBaseControl )
+import Control.Monad.Trans.Resource as Exports ( runResourceT )
+import Data.Aeson                   as Exports ( FromJSON
+                                               , Result( Error
+                                                       , Success
+                                                       )
+                                               , ToJSON
+                                               , Value
+                                               , fromJSON
+                                               , toJSON
+                                               )
+import Data.Time.Clock              as Exports ( UTCTime )
+import System.IO.Unsafe                        ( unsafePerformIO )
 
 debugLock :: MVar ()
 debugLock = unsafePerformIO $ newMVar ()
