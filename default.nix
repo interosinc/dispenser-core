60f681ea42e4a460759126c6b993f1a6c9ffeb63
John Evans <lgastako@gmail.com>
2018-10-06 00:25:23 -0600

    Varargs in default and shell.

--- default.nix
+++ default.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc843" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc843", ...}:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./dispenser-core.nix { }
