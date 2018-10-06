cb2da7aa1abf5364cbf6023bb5b4c9473f43d0ba
John Evans <lgastako@gmail.com>
2018-10-05 18:48:00 -0600

    Immediately following instrs

---
+++ dispenser-core.nix
@@ -0,0 +1,22 @@
+{ mkDerivation, aeson, aeson-diff, base, bytestring, containers
+, data-default, foldl, hspec, lens, monad-control, protolude
+, QuickCheck, quickcheck-instances, random, resourcet, stdenv, stm
+, streaming, text, time, unordered-containers, zero
+}:
+mkDerivation {
+  pname = "dispenser-core";
+  version = "0.1.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson aeson-diff base bytestring containers data-default foldl lens
+    monad-control protolude QuickCheck quickcheck-instances random
+    resourcet stm streaming text time unordered-containers zero
+  ];
+  testHaskellDepends = [
+    aeson aeson-diff base bytestring containers data-default foldl
+    hspec lens monad-control protolude QuickCheck quickcheck-instances
+    random resourcet stm streaming text time unordered-containers zero
+  ];
+  homepage = "https://github.com/superpowerscorp/dispenser-core#readme";
+  license = stdenv.lib.licenses.bsd3;
+}
